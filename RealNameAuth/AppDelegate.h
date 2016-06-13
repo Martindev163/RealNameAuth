d27593655c9c53a94f2e8de48e37bc7a0ae19c53
MHZ <wlsxmahaozhe@163.com>
2016-06-13 10:43:21 +0800

    Initial Commit

---
+++ RealNameAuth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealNameAuth
+//
+//  Created by 马浩哲 on 16/6/13.
+//  Copyright © 2016年 junanxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealNameAuth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealNameAuth
+//
+//  Created by 马浩哲 on 16/6/13.
+//  Copyright © 2016年 junanxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
